290f0d48ebe7c6903bc87be15d8b8416209bac9c
afroactuary <afroactuary@outlook.com>
2022-06-02 12:34:01 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#include <stdio.h>o
+#include <stdio.h>
 /**
  * main - Entry point
  *
@@ -9,3 +9,4 @@ int main(void)
 	puts("\"programming is like building a multilingual puzzle");
 	return (0);
 }
+
