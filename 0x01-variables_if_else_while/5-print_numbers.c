20c8254fc06c777c2a3cf00ae156d102a9a1139a
afroactuary <afroactuary@outlook.com>
2022-06-03 08:37:51 -0700

    Task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints the numbers since 0 to 9
+ * main - Prints single digits
  *
  * Return: Always (Success)
 */
@@ -9,7 +9,7 @@ int main(void)
 {
 	char n;
 
-	for (n = '0'; n = '9'; n++);
+	  (n = '0'; (n) = '9'; n++);
 	{
 		putchar(n);
 	}
