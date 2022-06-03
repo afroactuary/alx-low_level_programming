05b38bb1fc3d99955dee5d5a034d0617824d790d
afroactuary <afroactuary@outlook.com>
2022-06-03 09:48:45 -0700

    Task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 
 /**
- * main - Prints the numbers from 0-9.
- *
- * Return: Always (Success)
-*/
+  * main - Prints the numbers from 0-9.
+  *
+  * Return: Always (Success)
+  */
 int main(void)
 {
 	char n;
 
-	for (n = 0; (n) <= 9; n++)
+	for (n = '0'; n <= '9'; n++)
 	{
-		printf("%d", n);
 		putchar(n);
 	}
+
 	putchar('\n');
+
 	return (0);
 }
