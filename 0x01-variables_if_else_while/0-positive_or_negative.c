2baf1dbef8676600676d7bc4cb4f56c0759bcb19
afroactuary <afroactuary@outlook.com>
2022-06-03 06:19:13 -0700

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,11 @@
 #include <time.h>
 #include <stdio.h>
 
-/* betty style doc for function main goes there */
+/**
+ * main - Determines if number is positive, zero or negative
+ *
+ * Return: Always 0 (Success)
+*/
 int main(void)
 {
 	int n;
