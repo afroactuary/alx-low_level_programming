68926def66169acefcdb80ddb2f60938126891f5
afroactuary <afroactuary@outlook.com>
2022-06-07 11:52:53 -0700

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * peint_alphabet_x10 - prints 10 times the alphabet in lowercase followed by a new line
+ * peint_alphabet_x10 - prints 10 times the alphabet in lowercase
  */
 void print_alphabet_x10(void)
 {
 int i = 0;
 char c;
 
-while (i++ <=9)
+while (i++ <= 9)
 {
 for (c = 'a'; c <= 'z'; c++)
 _putchar(c);
