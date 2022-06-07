e3a52a9f37174b821f93bd37dc65d3861e104c9e
afroactuary <afroactuary@outlook.com>
2022-06-07 11:34:38 -0700

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,10 +6,9 @@
  */
 void print_alphabet(void)
 {
-	char  c;
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+_putchar(letter);
 
-	  (c = 'a'; c <= 'z'; c++)
-
-	_putchar(c);
-	_putchar('\n');
+_putchar('\n');
 }
