474d93537b3a41f021a0880c8e9060ef674c142b
afroactuary <afroactuary@outlook.com>
2022-06-08 14:27:05 -0700

    Task 1

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * causes an infinite loop
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+
+	while (i < 10)
+	{
+		putchar(i);
+	}
+	printf("Infinite loop incoming :(\n");
+
+	return (0);
+}
