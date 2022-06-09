743339cc3e820036fef7fb644ac52eb502138efb
afroactuary <afroactuary@outlook.com>
2022-06-09 04:51:02 -0700

    Task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isupper - checkes upper case
+ * @c: print int
+ * Return: give 1 or 0
+ */
+
+@c: print int
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+
+}
